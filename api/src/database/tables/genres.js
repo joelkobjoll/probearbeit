49802a2e14e8d796f5eeaec71f6e2feae3224304
Joel KObjoll <kobjoll@gmail.com>
2019-03-26 21:15:19 +0100

    Rename genre to renres

--- api/src/database/tables/genres.js
+++ api/src/database/tables/genres.js
@@ -2,11 +2,11 @@ import {db} from '@/database/connection'
 import Sequelize from 'sequelize'
 import epilogue from 'epilogue'
 
-export const modelGenre = db.define('genre', {
-    title: Sequelize.TEXT,
+export const modelGenre = db.define('genres', {
+    name: Sequelize.TEXT,
 })
 
 export const tableGenre = epilogue.resource({
     model: modelGenre,
-    endpoints: ['/genre', '/genre/:id'],
+    endpoints: ['/genres', '/genres/:id'],
 })
