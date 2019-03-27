4af4995dfd2b3d0ea8eb66ca7d897738366f4079
Joel KObjoll <kobjoll@gmail.com>
2019-03-27 19:24:40 +0100

    Update production config

--- api/src/server/server.prod.js
+++ api/src/server/server.prod.js
@@ -1,22 +1,10 @@
-import {db} from '../middleware/database';
-import bodyParser from 'body-parser'
-import cors from 'cors';
-import express from 'express'
-import path from 'path'
-import timeout from 'connect-timeout'
-import webpack from 'webpack'
+import {app} from '@/app';
+import {db} from '@/database/connection'
+import '@/database/tables';
+import '@/router'
 
-db.sync({
-    force: process.env.DB_FORCE_SYNC
-}).then(() => {
-    const app = express()
-    const DIST_DIR = __dirname
-    const API_PORT = process.env.PORT || 8081
+const API_PORT = process.env.PORT || 8080
 
-    app.use(cors())
-    app.use(bodyParser.json())
-    app.use(timeout('120s'))
-    app.use(express.static(DIST_DIR))
-
-    app.listen(API_PORT, () => console.log(`listening to port localhost:${API_PORT}`))
-})
+db
+.sync({force: false})
+.then(() => app.listen(API_PORT, () => console.log(`Listing to http://localhost:${API_PORT}`)))
